0ede1b24bfc1c1cc6101f9213ec392c20bbdc507
bosvs <eroinevs@gmail.com>
2024-12-12 01:15:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        class Pizza
+        {
+            string dough;
+            string sauce;
+            string topping;
+            public Pizza() { }
+            public void SetDough(string d) { dough = d; }
+            public void SetSauce(string s) { sauce = s; }
+            public void SetTopping(string t) { topping = t; }
+            public void Info()
+            {
+                Console.WriteLine("Dough: {0}\nSauce: {1}\nTopping: {2}",
+                    dough, sauce, topping);
+            }
+        }
+
+        // Abstract Builder
+        abstract class PizzaBuilder
+        {
+            protected Pizza pizza;
+            public PizzaBuilder() { }
+            public Pizza GetPizza() { return pizza; }
+            public void CreateNewPizza() { pizza = new Pizza(); }
+            public abstract void BuildDough();
+            public abstract void BuildSauce();
+            public abstract void BuildTopping();
+        }
+
+        // Concrete Builder for Hawaiian Pizza
+        class HawaiianPizzaBuilder : PizzaBuilder
+        {
+            public override void BuildDough() { pizza.SetDough("cross"); }
+            public override void BuildSauce() { pizza.SetSauce("mild"); }
+            public override void BuildTopping() { pizza.SetTopping("ham+pineapple"); }
+        }
+
+        // Concrete Builder for Spicy Pizza
+        class SpicyPizzaBuilder : PizzaBuilder
+        {
+            public override void BuildDough() { pizza.SetDough("pan baked"); }
+            public override void BuildSauce() { pizza.SetSauce("hot"); }
+            public override void BuildTopping() { pizza.SetTopping("pepperoni+salami"); }
+        }
+
+        // Concrete Builder for Margarita Pizza
+        class MargaritaPizzaBuilder : PizzaBuilder
+        {
+            public override void BuildDough() { pizza.SetDough("thin crust"); }
+            public override void BuildSauce() { pizza.SetSauce("tomato"); }
+            public override void BuildTopping() { pizza.SetTopping("mozzarella+fresh basil"); }
+        }
+
+        // Director
+        class Waiter
+        {
+            private PizzaBuilder pizzaBuilder;
+            public void SetPizzaBuilder(PizzaBuilder pb) { pizzaBuilder = pb; }
+            public Pizza GetPizza() { return pizzaBuilder.GetPizza(); }
+            public void ConstructPizza()
+            {
+                pizzaBuilder.CreateNewPizza();
+                pizzaBuilder.BuildDough();
+                pizzaBuilder.BuildSauce();
+                pizzaBuilder.BuildTopping();
+            }
+        }
+
+        // A customer ordering a pizza.
+        class BuilderExample
+        {
+            public static void Main(string[] args)
+            {
+                Waiter waiter = new Waiter();
+                PizzaBuilder hawaiianPizzaBuilder = new HawaiianPizzaBuilder();
+                PizzaBuilder spicyPizzaBuilder = new SpicyPizzaBuilder();
+                PizzaBuilder margaritaPizzaBuilder = new MargaritaPizzaBuilder();
+
+                // Customer orders a Hawaiian pizza
+                waiter.SetPizzaBuilder(hawaiianPizzaBuilder);
+                waiter.ConstructPizza();
+                Pizza pizza = waiter.GetPizza();
+                Console.WriteLine("Hawaiian Pizza:");
+                pizza.Info();
+                Console.WriteLine();
+
+                // Customer orders a Spicy pizza
+                waiter.SetPizzaBuilder(spicyPizzaBuilder);
+                waiter.ConstructPizza();
+                pizza = waiter.GetPizza();
+                Console.WriteLine("Spicy Pizza:");
+                pizza.Info();
+                Console.WriteLine();
+
+                // Customer orders a Margarita pizza
+                waiter.SetPizzaBuilder(margaritaPizzaBuilder);
+                waiter.ConstructPizza();
+                pizza = waiter.GetPizza();
+                Console.WriteLine("Margarita Pizza:");
+                pizza.Info();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
